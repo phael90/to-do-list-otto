7f8af46e9f93c8fafb55667b4b8416552d43ed0c
phael <raphaelugha@gmail.com>
2022-07-10 10:03:51 +0100

    feat: add input field and submit button

--- src/containers/ToDoList/index.js
+++ src/containers/ToDoList/index.js
@@ -1,5 +1,21 @@
+import { useState } from "react";
+
 const ToDoList = () => {
-  return <div>Todo List Rendered</div>;
+  const [newTaskName, setNewTaskName] = useState("");
+  return (
+    <div>
+      <input
+        name="new-task-name"
+        id="new-task-name"
+        type="text"
+        value={newTaskName}
+        onChange={(event) => {
+          setNewTaskName(event.target.value);
+        }}
+      />
+      <button type="submit">Add Task</button>
+    </div>
+  );
 };
 
 export default ToDoList;
